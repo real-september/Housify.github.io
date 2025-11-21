e440e06a9dd3ca30ff4bc4d969c2aa03f9761d2d
Yash Bisht <yashbisht.0210@gmail.com>
2025-11-21 19:31:29 +0530

    Load housing dataset in home view
    Added a function to load housing dataset and pass it to the home template.
    
    Signed-off-by: Yash Bisht <yashbisht.0210@gmail.com>

--- website/views.py
+++ website/views.py
@@ -1,9 +1,17 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+import pandas as pd
 
+# importing dataset for housing prices
+def load_data():
+    path_to_file = "/Users/yashmac/Documents/WebApp (Python)/website/data/housing_dataset.csv"
+    housing = pd.read_csv(path_to_file)
+
+    return housing.to_dict(orient='records')
 views = Blueprint('views', __name__)
 
 @views.route('/')
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    data = load_data()
+    return render_template("home.html", user=current_user, data=data)
